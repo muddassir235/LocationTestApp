1a2ea8745b06d9f4d495c3f2cc6a24cdab12fdba
Muddassir Ahmed Khan <muddassir.ahmed235@outlook.com>
2016-01-20 19:29:05 +0500

    Accesses the devices current location by adding code in the onConnected Callback and in the onLocationChanged Callback

--- app/src/main/java/com/example/muddassirahmed/locationstestapp/MainActivity.java
+++ app/src/main/java/com/example/muddassirahmed/locationstestapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.muddassirahmed.locationstestapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,13 +17,15 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     //A tag for putting in the logs so that the logs of this activity can be found easily and be more reader friendly
-    private final String LOG_TAG="LocationTestApp";
+    private final String LOG_TAG = "LocationTestApp";
+    private final int MY_PERMISSIONS_REQUEST=1;
 
     //we need an instance of the GoogleApiClient in order to use google play location services
     GoogleApiClient googleApiClient;
@@ -27,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     TextView locationTV;
 
     //An instance of LocationRequest in order to store out current location.
-    LocationRequest mLocation;
+    LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        locationTV=(TextView) findViewById(R.id.location_tv);
+        locationTV = (TextView) findViewById(R.id.location_tv);
         //in the onCreate method we need to instantiate our GoogleApiClient to out desired API,
         // bieng the goole location services API (LocationServices.API) in our case.
         if (googleApiClient == null) {
@@ -84,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     protected void onStart() {
         googleApiClient.connect();
         super.onStart();
+
     }
 
     //in the onStop method we need to diconnect our GoogleApiClient
@@ -96,16 +104,84 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     //once the GoogleApiClient gets connected we need to get our location
     @Override
     public void onConnected(Bundle bundle) {
+        Log.v(LOG_TAG, "Entered the onConnected method");
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return;
+        }
+        Log.v(LOG_TAG, "About to request Location");
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, mLocationRequest, this);
+        Log.v(LOG_TAG,"Requested Location");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.i(LOG_TAG,"Google Api Client connection suspended");
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(LOG_TAG,"Google Api Client connection failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v(LOG_TAG,location.toString());
+        locationTV.setText(location.toString());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    googleApiClient.connect();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
 
     }
 }
